66feaa7898831cc9860d9274c8b07213abf6191d
Techcraft7 <mctechcraft7@gmail.com>
2020-03-20 15:27:57 -0400

    Rewrote code and lots of comments

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,50 +1,54 @@
-#define L_TRIG 6        // Pin used for left trigger
-#define R_TRIG 3        // Pin used for right trigger
 #define L_JOYSTICK A0   // Pin used for left joystick
 #define R_JOYSTICK A3   // Pin used for right joystick
 
-int8_t speedLevel = 20; //Maximum speed (%) = speedLevel * 5 (units are percent)
+//You can tweak this value to your liking!
+
+#define SEND_DELAY 50
 
 void setup() {  
   Serial1.begin(9600); // Start serial communication with XBee at 9600 baud
-  delay(10);
-  
-  Serial1.print("W7001\r\n"); // Set the bit in enable register 0x70 to 0x01
-
-  pinMode(L_TRIG,INPUT_PULLUP); // Enable pullup resistor for left trigger
-  pinMode(R_TRIG,INPUT_PULLUP); // Enable pullup resistor for right trigger
 }
 
 void loop() {
-  int16_t leftStick, rightStick;    // We'll store the the analog joystick values here
-  char buf0[10],buf1[10]; // character buffers used to set motor speed
-  // Read joysticks
-  // Convert analog value range to motor speeds (in %)
-  leftStick = (5-(analogRead(L_JOYSTICK)/93))*speedLevel;
-  rightStick = (5-(analogRead(R_JOYSTICK)/93))*speedLevel;
-  // Build motor 0 buffer
-  if(leftStick > 0)
-  {
-    sprintf(buf0,"M0F%d\r\n",leftStick);
-  }
-  else
-  {
-    sprintf(buf0,"M0R%d\r\n",abs(leftStick));
-  }
 
-  // Build motor 1 buffer
-  if(rightStick > 0)
-  {
-    sprintf(buf1,"M1F%d\r\n",rightStick);
-  }
-  else
-  {
-    sprintf(buf1,"M1R%d\r\n",abs(rightStick));
-  }
+  //temporary values
+  
+  short leftStick;
+  short rightStick;
+  char buf0[6];
+  char buf1[6];
+
+  //joystick values go from 930 to 0
+  //full reverse is 930, full forward is 0
+  //divide by 930 to get a value between -1 and 0
+  //multiply by -200 to convert it to a value betwee -200 and 0
+  //add 100 to shift range to be between -100 and 100
+  
+  leftStick = ((analogRead(L_JOYSTICK) / 930) * -200) + 100;
+  rightStick = ((analogRead(R_JOYSTICK) / 930) * -200) + 100;
+
+  //add a '+' or a '-' to tell if the number is positive or negative
+  //instead of using an if statement here, I used an inline if
+  //the syntax for an inline if is as follows:
+  //condition ? value if true : value if false
+  //For example, the code 2 + 2 == 4 ? "yes" : "no"
+  //would be "yes" because the condition "2 + 2 == 4" is true
+  
+  char isLeftPositive = leftStick >= 0 ? '+' : '-';
+  char isRightPositive = rightStick >= 0 ? '+' : '-';
+
+  //put values in buffer
+  //sprinf() takes at least 2 paramaters
+  //the first is the buffer you want to write to
+  //the second is the format string you want to write to the buffer
+  //see http://www.cplusplus.com/reference/cstdio/printf/ for format charatcers
+  
+  sprintf(buf0,"L%s%03d\n", isLeftPositive, abs(leftStick));
+  sprintf(buf1,"R%s%03d\n", isRightPositive, abs(rightStick));
 
   // Send motor speeds
-  delay(50);
+  delay(SEND_DELAY);
   Serial1.print(buf0);
-  delay(50);
+  delay(SEND_DELAY);
   Serial1.print(buf1);
 }
--- Mega_Decoder/Mega_Decoder.ino
+++ Mega_Decoder/Mega_Decoder.ino
@@ -1,108 +1,216 @@
-#include <Servo.h>  
+#include <Servo.h>
+
+//constants
 
 #define MOTORL 5
 #define MOTORR 6
+#define MESSAGE_LEN 6
 
-char incomingByte[10];  
-int loc = 0;
+//servos
 Servo left;
 Servo right;
-int motors[2];
 
 void setup() {
-  // initialize both serial ports:
-  Serial.begin(9600);
-  Serial3.begin(9600);
-  Serial.println("Starting");
+
+  //init serial
+
+  Serial.begin(9600); //debug log
+  Serial3.begin(9600); //x bee
+
+  //attach servos
   do {
     left.attach(MOTORL);
     right.attach(MOTORR);
   }
   while (!left.attached() || !right.attached());
   Serial.println("Attached all servos!");
-}
 
-int lStick = 0;
-int rStick = 0;
-String code = "";
-int time = 0;
-
-void loop() {
+  // set pin 52 and 53 to high (power for PWM motors)
   for (int i = 52; i <= 53; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
   }
-  if(Serial3.available()){
-    incomingByte[loc] = Serial3.read();
-    String debug = "";
-    if (incomingByte[loc] < 32 && incomingByte[loc] < 127) {
-      debug = "Control character: " + String((int)incomingByte[loc]);
+  Serial.println("Starting");
+}
+
+char message[MESSAGE_LEN];
+int offset = 0;
+
+//main loop
+
+void loop() {
+
+  //reset offset once we get to the end of a message
+
+  if (offset == MESSAGE_LEN) {
+
+    //see if we recieved a left motor set
+
+    bool setLeft = message[0] == 'L';
+
+    //if the first character is NOT 'L', make sure it is an 'R'
+
+    if (!setLeft) {
+      if (message[0] != 'R') {
+
+        //invalid data was recieved, so exit
+
+        return;
+      }
     } else {
-      debug = "Character \'" + String(incomingByte[loc]) + "\'";
+      if (message[0] != 'L') {
+
+        //invalid data was recieved, so exit
+
+        return;
+      }
     }
-    //Serial.println("Got " + debug);
-    code = String(code + incomingByte[loc]);
-    loc++;
+
+    //read values
+    //strstr takes 2 parameters
+    //the first is the string to search
+    //the second is the string to search for
+    //returns NULL if it cant find it
+    //For example, strstr("L+123", "+") returns "+123"
+    //However, strstr("L+123", "-") returns NULL!
+
+    char *val = strstr(message, "+");
+
+    //check if val is null
+
+    if (val == NULL) {
+      val = strstr(message, "-");
+    }
+
+    //check to see if val is still NULL
+    //only happens if there is a data corruption
+
+    if (val == NULL) {
+
+    }
+
+    //atoi() converts a string to an integer
+    //For example, atoi("-123") returns -123,
+    //However, atoi("123abc456") returns 123!
+    //atoi() reads until the first non number or the end
+
+    int speed = atoi(val);
+
+    //Next, limit speed from -100 to 100
+    //This is in case we get data corruption and we get "L+999" as our message
+
+    //constrain() takes 3 values
+    //the first is the value you want to limit
+    //the second is the minimum value
+    //the thrid is the maximum value
+    //For example, constrain(12345678900, -100, 100) should return 100
+    //However, since 12345678900 is WAY more than the 32-bit-signed integer limit (2,147,483,648)
+    //it will actually return -100!
+    //when 12345678900 is converted to a 32 bit integer, it becomes -539222988
+    //in binary, 12345678900 is:
+    //1011011111110111000001110000110100
+    //in binary, -539222988 is:
+    //11011111110111000001110000110100
+    //if you line them up you get:
+    //1011011111110111000001110000110100 = 12345678900
+    //  11011111110111000001110000110100 =  -539222988
+    //When converting, it chops off all of the extra bits,
+    //and because the left most bit in -539222988 is a 1,
+    //that means that it is a negative!
+    //if that one bit was flipped, it would be positive
+
+    speed = constrain(speed, -100, 100);
+
+    //Then, convert speed to a PWM value
+
+    speed = getPWMSpeed(speed);
+
+    //Finally, set the motors
+
+    if (setLeft) {
+      left.writeMicroseconds(speed);
+    } else {
+      right.writeMicroseconds(speed);
+    }
+
+    //set offset back to 0 to start a new message
+
+    offset = 0;
   }
-  if (incomingByte[loc - 1] == 'W')  { // skip 6 more bytes
-    int v = -1;
-    for (int i = 0; i <= 5; i++) {
-      dump();
-      //Serial.println("Skipping init code");
+
+  //if data is available
+
+  if (Serial3.available()) {
+
+    //read one byte from Serial3
+
+    message[offset] = Serial3.read();
+
+    //check for sync issues!
+    //we want the first character of our message to be 'L' or 'R',
+    //so if it isnt we can have issues.
+    //this code block reads to the end of the message and waits for a new one!
+    //the end of our message is a '\n' (line feed)
+
+    if (offset == 0 && (message[0] != 'L' || message[0] != 'R')) {
+
+      //wait for \n (end of message)
+
+      char b = '\0';
+
+      while (b != '\n') {
+        if (Serial3.available()) {
+          b = Serial3.read();
+        }
+      }
+
+      //exit loop and wait for next message
+
+      return;
     }
-    incomingByte[0] = '\0';
-    loc = 0;
-    code = "";
-  }
-  if (incomingByte[loc - 1] == 13) { //end of transmission
-    //decode
-    dump();
-    loc = 0;
-    int stickID = incomingByte[1] - 48;
-    //Serial.println("Decoding for stick " + String(stickID));
-    String digits = "";
-    boolean forward = incomingByte[2] == 'F';
-    for (int i = 3; incomingByte[i] != '\r'; i++) {
-       digits += incomingByte[i];
-    }
-    int s = digits.toInt();
-    if (s > 100) {
-      s = 100;
-    }
-    if (forward) { //F and R are switched!
-      s *= -1;
-    }
-    for (int i = 0; i < strlen(incomingByte); i++) {
-      incomingByte[i] = 0;
-    }
-    motors[1 - stickID] = s;
-    time++;
-  }
-  if (time == 2) {
-    time = 0; 
-    left.writeMicroseconds(getPWMSpeed(motors[0]));
-    right.writeMicroseconds(getPWMSpeed(motors[1]));
-    Serial.println("Left: " + String(motors[0]) + " Right: " + String(motors[1]) + " PWML: " + String(getPWMSpeed(motors[0]))
-    + " PWMR: " + String(getPWMSpeed(motors[1])) + " Motor L: " + String(left.read()) + " Motor R: " + String(right.read()));
+
+    //increase offset
+
+    offset++;
   }
 }
+
+//this converts a value between -100 and 100 to valid PWM values
+
 int getPWMSpeed(int x) {
+
+  //make x positive but remeber if it was negative
   boolean neg = x < 0;
-  if (neg) {
-    x *= -1;
-  }
+  x = abs(x);
+
   if (x == 0) {
     return 1500;
   }
+
   //1000 to 1460 = reverse
   //1500 = stop
   //1540 to 2000 = forward
+  //scaled is out PWM value
+
   double scaled = ((x / 100.0) * 460.0) + 1540.0;
+
+  //if it was negative, shift range from 1540 to 2000 to 1000 to 1540
+
   if (neg) {
     scaled -= 540.0;
   }
+
+  //return PWM value
+
   return (int)scaled;
 }
+
+//this skips the next incoming byte
+
 void dump() {
-  while (Serial3.read() < 0);
+
+  //loop until you recieve a byte and then read it
+
+  while (!Serial3.available());
+  Serial3.read();
 }
