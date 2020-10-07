816fd33bd84b340ad5ff86626ce053b075999367
brent-hollers <72216475+brent-hollers@users.noreply.github.com>
2020-10-07 10:01:05 -0400

    Updated motor controls to use servo class and lowered power settings so that wheels are slowed. Tested and working on both motor controllers, but has delay, suggest upping baud rate in serial on both files and reworking if/else. Also need additional commenting and cleanup of code.

--- ArduinoReceiver/ArduinoReceiver.ino
+++ ArduinoReceiver/ArduinoReceiver.ino
@@ -1,40 +1,54 @@
-int leftMotor = 5;
-int rightMotor = 6;
+//A delay of 1000 to 1460 Microseconds is Proportional Reverse
+//A delay of 1460 to 1540 Microseconds is neutral
+//A delay of 1540 to 2000 Microseconds is Proportional Forward
+//A delay of 2000 Microseconds is Full Forward
 
-void setup() {
-  pinMode(leftMotor, OUTPUT);
-  pinMode(rightMotor, OUTPUT);
-  
- Serial.begin(9600);
+#include <Servo.h>
+
+int LeftPin = 6;      //Left Motor pin
+Servo LeftMotor;           //Left Motor Servo Object
+
+int RightPin = 5;      //Right Motor pin
+Servo RightMotor;           //Right Motor Servo Object
+
+void setup()
+{
+  // Tells each of the servo objects which pin it should output to
+  LeftMotor.attach(LeftPin);
+  RightMotor.attach(RightPin);
+
+  Serial.begin(9600);
+  LeftMotor.writeMicroseconds(1500);          //right motor driver code
+  RightMotor.writeMicroseconds(1500);          //left motor driver code
 }
 
 void loop() {
   if(Serial.available() > 0){
-    int xbee = Serial.read();
+    int xbee=Serial.read();
     
     if(xbee == 65){
-      analogWrite(leftMotor, 255);
-      analogWrite(rightMotor, 255);
+      LeftMotor.writeMicroseconds(1800);          //right motor driver code
+      RightMotor.writeMicroseconds(1800);          //left motor driver code
       Serial.println("Forward");
     }
     else if(xbee == 66){
-      analogWrite(leftMotor, 255);
-      analogWrite(rightMotor, 100);
+      LeftMotor.writeMicroseconds(1800);          //right motor driver code
+      RightMotor.writeMicroseconds(1200);          //left motor driver code
       Serial.println("Turn Right");
     }
     else if(xbee == 67){
-      analogWrite(leftMotor, 100);
-      analogWrite(rightMotor, 100);
+      LeftMotor.writeMicroseconds(1200);          //right motor driver code
+      RightMotor.writeMicroseconds(1200);          //left motor driver code
       Serial.println("Reverse");
     }
     else if(xbee == 68){
-      analogWrite(leftMotor, 100);
-      analogWrite(rightMotor, 255);
+      LeftMotor.writeMicroseconds(1200);          //right motor driver code
+      RightMotor.writeMicroseconds(1800);          //left motor driver code
       Serial.println("Turn Left");
     }
     else if(xbee == 69){
-      analogWrite(leftMotor, 150);
-      analogWrite(rightMotor, 150);
+      LeftMotor.writeMicroseconds(1500);          //right motor driver code
+      RightMotor.writeMicroseconds(1500);          //left motor driver code
       Serial.println("Stop");
     }
     else{
