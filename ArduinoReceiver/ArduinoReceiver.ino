373c8c77463d1fd64466ece7437a8f43053fce33
brent-hollers <72216475+brent-hollers@users.noreply.github.com>
2020-10-15 14:20:22 -0400

    This reverts code back to original functioning code on both sender and receiver files

--- ArduinoReceiver/ArduinoReceiver.ino
+++ ArduinoReceiver/ArduinoReceiver.ino
@@ -4,76 +4,23 @@
 
 #include <Servo.h>
 
-#define IS_SEPERATOR(v) IS_L(v) || IS_R(v)
-#define IS_L(v) v == 0x4C // ASCII for L
-#define IS_R(v) v == 0x52 // ASCII for R
-
-
 #define PIN_L 6
 #define PIN_R 5
-#define MESSAGE_SIZE 3
 
-<<<<<<< HEAD
 Servo leftMotor;      // Left Motor Servo Object
 Servo rightMotor;     // Right Motor Servo Object
 
-<<<<<<< HEAD
-int L, R;
-unsigned char* data = (unsigned char*)malloc(MESSAGE_SIZE); // Use malloc to get 3 bytes of RAM; Data: L/R + high + low
 
-=======
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
-=======
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
 void setup()
 {
   // Tells each of the servo objects which pin it should output to
   leftMotor.attach(PIN_L);
   rightMotor.attach(PIN_R);
 
-<<<<<<< HEAD
   Serial.begin(9600); // Init serial
   // Set default values because 0 means full reverse
-  L = 1500;
-  R = 1500;
-
-  memset(data, 0x00, MESSAGE_SIZE);
-}
-
-void loop() {
-  if(Serial.available() >= MESSAGE_SIZE){
-    Serial.readBytes(data, MESSAGE_SIZE);
-
-    // Only read message if start is 'L' or 'R'
-    if (IS_SEPERATOR(data[0])) {
-      int high = data[1];
-      int low = data[2];
-      //HHHHHHHHLLLLLLLL
-      int pwm = constrain(((high << 8) | low) - 512, -500, 500) + 1500; // Clamp between -500 and 500, then add 1500 to put it in range
-      if (IS_L(data[0])) { // Set left
-        L = pwm;
-      } else if (IS_R(data[0])) { // Set right
-        R = pwm;
-      }
-    } else {
-      unsigned char* temp = (unsigned char *)malloc(1); // Get 1 byte of RAM
-      while (!IS_SEPERATOR(temp[0])) {
-        temp = Serial.readBytes(temp, 1);
-      }
-      free(temp); // Clean up RAM because we don't need it anymore
-    }
-
-    // Use memset to copy MESSAGE_SIZE 0x00's to data
-    // Just sets all the values in data to 0x00 (which is zero)
-    memset(data, 0x00, MESSAGE_SIZE); // Reset message data
-  }
-  leftMotor.writeMicroseconds(L);
-  rightMotor.writeMicroseconds(R);
-}
-=======
-  Serial.begin(9600);
-  LeftMotor.writeMicroseconds(1500);          //right motor driver code
-  RightMotor.writeMicroseconds(1500);          //left motor driver code
+  leftMotor.writeMicroseconds(1500);          //right motor driver code
+  rightMotor.writeMicroseconds(1500);          //left motor driver code
 }
 
 void loop() {
@@ -81,37 +28,32 @@ void loop() {
     int xbee=Serial.read();
     
     if(xbee == 65){
-      LeftMotor.writeMicroseconds(1800);          //right motor driver code
-      RightMotor.writeMicroseconds(1800);          //left motor driver code
+      leftMotor.writeMicroseconds(1800);          //right motor driver code
+      rightMotor.writeMicroseconds(1800);          //left motor driver code
       Serial.println("Forward");
     }
     else if(xbee == 66){
-      LeftMotor.writeMicroseconds(1800);          //right motor driver code
-      RightMotor.writeMicroseconds(1200);          //left motor driver code
+      leftMotor.writeMicroseconds(1800);          //right motor driver code
+      rightMotor.writeMicroseconds(1200);          //left motor driver code
       Serial.println("Turn Right");
     }
     else if(xbee == 67){
-      LeftMotor.writeMicroseconds(1200);          //right motor driver code
-      RightMotor.writeMicroseconds(1200);          //left motor driver code
+      leftMotor.writeMicroseconds(1200);          //right motor driver code
+      rightMotor.writeMicroseconds(1200);          //left motor driver code
       Serial.println("Reverse");
     }
     else if(xbee == 68){
-      LeftMotor.writeMicroseconds(1200);          //right motor driver code
-      RightMotor.writeMicroseconds(1800);          //left motor driver code
+      leftMotor.writeMicroseconds(1200);          //right motor driver code
+      rightMotor.writeMicroseconds(1800);          //left motor driver code
       Serial.println("Turn Left");
     }
     else if(xbee == 69){
-      LeftMotor.writeMicroseconds(1500);          //right motor driver code
-      RightMotor.writeMicroseconds(1500);          //left motor driver code
+      leftMotor.writeMicroseconds(1500);          //right motor driver code
+      rightMotor.writeMicroseconds(1500);          //left motor driver code
       Serial.println("Stop");
     }
     else{
       Serial.println("Out of Range");
     }
   }
-<<<<<<< HEAD
 }
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
-=======
-}
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
--- SparkfunControllerSimple/SparkfunControllerSimple.ino
+++ SparkfunControllerSimple/SparkfunControllerSimple.ino
@@ -1,83 +1,39 @@
 
-<<<<<<< HEAD
+
 #define PIN_L A3   // Pin used for left joystick
 #define PIN_R A0   // Pin used for right joystick
-
-#define MESSAGE_SIZE 3
-
-unsigned char* data = (unsigned char*)malloc(MESSAGE_SIZE); // Get 3 bytes of RAM
-=======
 #define L_TRIG 6        // Pin used for left trigger
 #define R_TRIG 3        // Pin used for right trigger
 #define L_JOYSTICK A3   // Pin used for left joystick
 #define R_JOYSTICK A0   // Pin used for right joystick
 
-<<<<<<< HEAD
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
-=======
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
-
 void setup() {  
   Serial1.begin(9600); // Start serial communication with XBee at 9600 baud
   delay(10);
   Serial1.print("W7001\r\n"); // Set the bit in enable register 0x70 to 0x01
-
-  memset(data, 0x00, MESSAGE_SIZE); // Reset data to all zeroes
 }
 
-void loop() {1
-  int L, R;
-
+void loop() {
+  int16_t leftStick, rightStick;    // We'll store the the analog joystick values here
   // Read joysticks
-<<<<<<< HEAD
-  L = analogRead(PIN_L);
-  R = analogRead(PIN_R);
-
-  // Send data
-  
-  data[0] = 'L';
-  // HHHHHHHHLLLLLLLL
-  data[1] = L & (0xFF << 8); // get only H
-  data[2] = L & 0xFF; // get only L
-
-  Serial1.write(data, MESSAGE_SIZE);
-  memset(data, 0x00, MESSAGE_SIZE); // Reset data, redundant but just to be safe
-
-  data[0] = 'R';
-  data[1] = R & (0xFF << 8);
-  data[2] = R & 0xFF;
-
-  Serial1.write(data, MESSAGE_SIZE);
-  
-  delay(25); // Delay to not overload xbee
-
-  memset(data, 0x00, MESSAGE_SIZE); // Reset data to all zeroes
-}
-=======
   // Convert analog value range to motor speeds (in %)
   leftStick = analogRead(L_JOYSTICK); //~1023 up, ~7-9 down
   rightStick = analogRead(R_JOYSTICK); //~1023 up, ~7-9 down
 
    if (leftStick > 1000 && rightStick > 1000) {
-      SerialUSB.println("Forward");
-      Serial1.print('A');
+      Serial1.print('A'); 
     }
     else if ( leftStick > 1000 && rightStick < 400) {
-     SerialUSB.println("Turn Right");
       Serial1.print('B');
     }
     else if (leftStick < 400 && rightStick < 400) {
-      SerialUSB.println("Reverse");
       Serial1.print('C');
     }
     else if (leftStick < 400 && rightStick > 1000) {
-      SerialUSB.println("Turn Left");
       Serial1.print('D');
     }
     else {
-      SerialUSB.println("Stop");
       Serial1.print('E');
     }
     delay(50);
 }//end loop
->>>>>>> parent of 5994b76... Merge branch 'master' of https://github.com/sequoyahfrc/mobility-wheelchair into master
